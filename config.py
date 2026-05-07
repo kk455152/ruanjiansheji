4edc63d6b2d8e5347cf2be93d79bea3b3d384901
Gqiany <2697906439@qq.com>
2026-05-07 14:27:27 +0800

    Add configuration for MySQL and MongoDB

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MYSQL_CONFIG = {
+    "host": os.getenv("MYSQL_HOST", "127.0.0.1"),
+    "port": int(os.getenv("MYSQL_PORT", "3306")),
+    "user": os.getenv("MYSQL_USER", "root"),
+    "password": os.getenv("MYSQL_PASSWORD", ""),
+    "database": os.getenv("MYSQL_DB", "music_player"),
+    "charset": "utf8mb4",
+}
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://127.0.0.1:27017")
+MONGO_DB = os.getenv("MONGO_DB", "music_player")
+
+TOKEN_EXPIRE_HOURS = int(os.getenv("TOKEN_EXPIRE_HOURS", "72"))
